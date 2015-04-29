cc39979ea6d4cbcbbc450a8c6319ce6c7100d090
JD <jd@csh.rit.edu>
2015-04-29 10:07:42 -0400

    Now pulls from both the old and new data from hbase

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,14 +5,14 @@ require "open-uri"
 class ApiController < ApplicationController
     def get_index
         result = []
-        
+
         begin
             options = { :headers => 
                         { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}, 
-                        :timeout => 2 }
+                        :timeout => 10 }
             response = HTTParty.get(
                 "http://namenode.csh.rit.edu:20550/overview/pageviews/timestamps", options)
-            
+
             data = JSON.parse(response.body)
 
             data['Row'][0]['Cell'].each do |cell|
@@ -26,7 +26,7 @@ class ApiController < ApplicationController
         end
         render json: result.to_json, :callback => params['callback']
     end
-    
+
     def get_page
         page_title = Rack::Utils.escape(params[:page_title])
         result = []
@@ -34,23 +34,32 @@ class ApiController < ApplicationController
         begin
             options = { :headers => 
                         { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}, 
-                        :timeout => 7}
+                        :timeout => 10}
             response = HTTParty.get(
-                "http://namenode.csh.rit.edu:20550/pages/#{page_title}/timestamps", options)
-            
-            data = JSON.parse(response.body)
+                "http://namenode.csh.rit.edu:20550/pages/#{page_title}/test", options)
 
+            data = JSON.parse(response.body)
+            data['Row'][0]['Cell'].each do |cell|
+                cell = Base64.decode64(cell['$'])
+                cell.split(",").each do |elem|
+                  split = elem.split("-")
+                  result << [split[0].to_i * 1000, split[1].to_i]
+                end
+            end
+
+           response = HTTParty.get(
+                "http://namenode.csh.rit.edu:20550/pages/#{page_title}/timestamps", options)
+
+            data = JSON.parse(response.body)
             data['Row'][0]['Cell'].each do |cell|
                 timestamp = Base64.decode64(cell['column']).split(":")[1].to_i * 1000
                 count = Base64.decode64(cell['$']).to_i
                 result << [timestamp, count]
             end
-
         rescue Net::ReadTimeout
             Rails.logger.error "HTTP timeout to hbase fetching #{page_title}"
         end
         render json: result.to_json, :callback => params['callback']
-        
 
     end
 end
